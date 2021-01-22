3870abf89ef1d4e4575327fc6c9814cb54887e39
Mahbod Afarin <mahbodafarin1admin@Rajivs-MacBook-Air.local>
2021-01-21 21:37:36 -0800

    u

--- shared/graph.cu
+++ shared/graph.cu
@@ -101,7 +101,7 @@ void Graph<E>::ReadGraph()
 			uint *outDegreeCounter  = new uint[num_nodes];
 			uint location; 
 			cout << "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" << endl;			 
-			for(uint i=0; i<30622564-1000000; i++)
+			for(uint i=0; i<30622564-1500000; i++)
 			{
 				location = nodePointer[edges[i].source] + outDegreeCounter[edges[i].source];
 				edgeList[location].end = edges[i].end;
